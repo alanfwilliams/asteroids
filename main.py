1c0b0f94d73bc1a439c80942c0f424da0d3fba08
Alan Williams <astropiloto@gmail.com>
2025-03-22 21:18:54 -0500

    generate player and asteroids

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from asteroidfield import *
+from player import *
 import pygame
 from constants import *
+from asteroid import *
 
 def main():
     pygame.init()
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    clock = pygame.time.Clock()
+    dt = 0
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+    player = Player(player_x, player_y)
+    asteroidfield = AsteroidField()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,7 +30,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dnum = clock.tick(60)
+        dt = dnum / 1000
 
 if __name__ == "__main__":
     main()
